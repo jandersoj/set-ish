bd7fbf359e88b9e6bba32d7c6fd1d34819b57a8a
jandersoj <jandersoj03@gmail.com>
2026-05-11 13:27:52 -0400

    trying to make it deploy right

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import './globals.css';
 export const metadata = {
   title: 'SET Game',
   description: 'Find SETs! A card game built with Next.js',
+  viewport: 'width=device-width, initial-scale=1.0',
 };
 
 export default function RootLayout({ children }) {
